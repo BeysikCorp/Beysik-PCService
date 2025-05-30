cd8477fbfe394decb1afc586ac18a7c78cd7d2df
Trent <65882951+Dre-OS@users.noreply.github.com>
2025-05-30 15:46:08 +0800

    fixed gitignore

---
+++ Beysik-PCService/Models/Product.Models.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Beysik_PCService.Models
+{
+    public class ProductDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string ProductsCollectionName { get; set; } = null!;
+    }
+
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("Brand")]
+        public string Brand { get; set; } = null!;
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+
+        [BsonElement("Size")]
+        public string Size { get; set; } = null!;
+
+        [BsonElement("Description")]
+        public string Description { get; set; } = null!;
+
+        [BsonElement("Category")]
+        public string Category { get; set; } = null!;
+
+        [BsonElement("Stock")]
+        public int Stock { get; set; }
+
+        [BsonElement("ImageUrl")]
+        public string ImageUrl { get; set; } = null!;
+
+        [BsonElement("Tags")]
+        public List<string> Tags { get; set; } = new List<string>();
+    }
+}
